1124692605248f255d93859c01af9591b3a61341
sobol_mobol <vicasob9@gmail.com>
2024-11-25 23:40:45 +0700

    all templates are done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mustacheExpress = require('mustache-express');
 const indexRoutes = require('./routes/index');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.engine('mustache', mustacheExpress(path.join(__dirname, '/views/partials'), '.mustache'));
 app.set('view engine', 'mustache');
@@ -13,7 +14,9 @@ app.set('views', path.join(__dirname, '/views'));
 app.use('/', indexRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(bodyParser.json());
 app.listen(3000, () => {
     console.log('server is running');
 });
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,24 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-function loadData(fileName){
-    const filePath = path.join(__dirname, '..', `data\\${fileName}.json`);
-    console.log(filePath);
-    const data = fs.readFileSync(filePath, 'utf-8');
-    return JSON.parse(data);
+function loadData(...fileNames) {
+    if (fileNames.length === 1) {
+        const filePath = path.join(__dirname, '..', 'data', `${fileNames[0]}.json`);
+        console.log(filePath);
+        const data = fs.readFileSync(filePath, 'utf-8');
+        return JSON.parse(data);
+    } else {
+        let data = {};
+        for (const fileName of fileNames) {
+            const filePath = path.join(__dirname, '..', 'data', `${fileName}.json`);
+            console.log(filePath);
+            const fileData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+            data = Object.assign(data, fileData); 
+        }
+        return data;
+    }
 }
+
 
 router.get("/", function(request, response){
     const data = loadData('main');
@@ -26,4 +38,13 @@ router.get("/gallery", function(request, response){
     const data = loadData('gallery');
     response.render('gallery', data);
 });
+router.get("/contact", function(request, response){
+    const data = loadData('contact', 'reviews');
+    response.render('contact', data);
+});
+router.post('/contact', (req, res) => {
+    console.log(req.body); 
+    res.send('Данные получены');
+});
+
 module.exports = router;
